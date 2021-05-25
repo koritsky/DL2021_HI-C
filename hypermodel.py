38f1bff62d9d6a6f51e783e210b7a0d683a243af
MarkZaretskiy <mark.zaretskiy@phystech.edu>
2021-05-25 14:57:40 +0300

    changed akita_loss

--- hypermodel.py
+++ hypermodel.py
@@ -17,8 +17,9 @@ neptune_logger = NeptuneLogger(
             api_key='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI3YTY4ZWY2ZC1jNzQxLTQ1ZTctYTM2My03YTZhNDQ5MTRlNzYifQ==',
             tags=['test_hypermodel_logging']
         )
-#from akita import 
-#from vehicle import 
+
+#from akita import Akita
+#from vehicle import Vehicle
 
 class Dummy(nn.Module):
     def __init__(self):
@@ -88,7 +89,7 @@ class HyperModel(pl.LightningModule):
 
     def _step(self, batch):
 
-        sequence, low_img, high_img = batch
+        sequence, low_img, high_img_akita, high_img_vehicle = batch
 
         akita_output = self.akita_forward(sequence)
         vehicle_output = self.vehicle_forward(low_img)
@@ -96,14 +97,14 @@ class HyperModel(pl.LightningModule):
         combined_input = torch.cat([akita_output, vehicle_output], dim=1) #stack along the channel dimension
         output = self.head(combined_input)
         
-        akita_normalized_output = (akita_output.detach() + 2) / 4
-        akita_loss = F.mse_loss(akita_normalized_output, high_img)
-        vehicle_loss = F.mse_loss(vehicle_output.detach(), high_img)
-        final_loss = F.mse_loss(output, high_img) #will be passed for backward
+        #akita_normalized_output = (akita_output.detach() + 2) / 4
+        akita_loss = F.mse_loss(akita_output.detach(), high_img_akita)
+        vehicle_loss = F.mse_loss(vehicle_output.detach(), high_img_vehicle)
+        final_loss = F.mse_loss(output, high_img_vehicle) #will be passed for backward
 
-        akita_metrics = self.calculate_metrics(akita_normalized_output, high_img)
-        vehicle_metrics = self.calculate_metrics(vehicle_output.detach(), high_img)
-        final_metrics = self.calculate_metrics(output.detach(), high_img)
+        akita_metrics = self.calculate_metrics(akita_output, high_img_akita)
+        vehicle_metrics = self.calculate_metrics(vehicle_output.detach(), high_img_vehicle)
+        final_metrics = self.calculate_metrics(output.detach(), high_img_vehicle)
         
         return {"loss":final_loss,
                 "akita_loss":akita_loss,
@@ -111,10 +112,11 @@ class HyperModel(pl.LightningModule):
                 "final_metrics":final_metrics,
                 "akita_metrics":akita_metrics,
                 "vehicle_metrics":vehicle_metrics,
-                "akita_output":akita_normalized_output,
+                "akita_output":akita_output.detach(),
                 "vehicle_output":vehicle_output.detach(),
                 "final_output":output.detach(),
-                "high_img":high_img
+                "high_img":high_img_vehicle,
+                "high_img_akita":high_img_akita,
                 }
         
     def _epoch_logging(self, outputs, phase):
@@ -127,17 +129,19 @@ class HyperModel(pl.LightningModule):
         final_output = torch.cat([o['vehicle_output'] for o in outputs[:3]], dim=0).cpu()
         vehicle_output = torch.cat([o['final_output'] for o in outputs[:3]], dim=0).cpu()
         high_imgs = torch.cat([o['high_img'] for o in outputs[:3]], dim=0).cpu()
+        high_imgs_akita = torch.cat([o['high_img_akita'] for o in outputs[:3]], dim=0).cpu()
         
         ###logging####
-        self.logger.experiment.log_metric('{}/loss'.format(phase), x=self.current_epoch, y=averaged_loss)
-        self.logger.experiment.log_metric('{}/akita_loss'.format(phase), x=self.current_epoch,  y=averaged_akita_loss)
-        self.logger.experiment.log_metric('{}/vehicle_loss'.format(phase), x=self.current_epoch, y=averaged_vehicle_loss)
+        self.logger.experiment.log_metric('{}/loss'.format(phase), averaged_loss) # x=self.current_epoch, y=averaged_loss)
+        self.logger.experiment.log_metric('{}/akita_loss'.format(phase),averaged_akita_loss) # x=self.current_epoch,  y=averaged_akita_loss)
+        self.logger.experiment.log_metric('{}/vehicle_loss'.format(phase), averaged_vehicle_loss) # x=self.current_epoch, y=averaged_vehicle_loss)
 
         ###save images###
         self.log_pictures(akita_img=akita_output,
                           vehicle_img=vehicle_output,
                           final_img=final_output,
                           high_img=high_imgs,
+                          high_img_akita=high_imgs_akita,
                           phase=phase)
     
     def _construct_grid(self, img):
@@ -146,24 +150,29 @@ class HyperModel(pl.LightningModule):
         fig, ax = plt.subplots(1, bs, sharey=True)
         for i in range(bs):
             ax[i].imshow(img[i])
-            ax[i].set_title("Place for metrics", fontsize=8)
+            #ax[i].set_title("Place for metrics", fontsize=8)
 
-            ax[i].text(5, 5, 'your legend', bbox={'facecolor': 'white', 'pad': 10})
+            ax[i].text(0, 0, 'your legend', bbox={'facecolor': 'white', 'pad': 10})
         
         return fig
 
-    def log_pictures(self, akita_img, vehicle_img, final_img, high_img, phase):
+    def log_pictures(self, akita_img, vehicle_img, final_img, high_img, high_img_akita, phase):
         
         grid = self._construct_grid(self.get_colors(final_img)) #utils.make_grid(self.get_colors(final_img), nrow=2)
         self.logger.experiment.log_image('{}/final_img'.format(phase), self.current_epoch, grid)
         
-        grid = self._construct_grid(self.get_colors(akita_img)) #utils.make_grid(self.get_colors(akita_img), nrow=2)
+        akita_img_normalized = (akita_img + 2) / 4
+        grid = self._construct_grid(self.get_colors(akita_img_normalized)) #utils.make_grid(self.get_colors(akita_img), nrow=2)
         self.logger.experiment.log_image('{}/akita_img'.format(phase), self.current_epoch,  grid)
         
         grid = self._construct_grid(self.get_colors(vehicle_img)) #utils.make_grid(self.get_colors(vehicle_img), nrow=2)
         self.logger.experiment.log_image('{}/vehicle_img'.format(phase), self.current_epoch, grid)
 
         grid = self._construct_grid(self.get_colors(high_img)) #utils.make_grid(self.get_colors(high_img), nrow=2)
+        self.logger.experiment.log_image('{}/high_img'.format(phase), self.current_epoch, grid)
+
+        high_img_akita_normalized = (high_img_akita + 2) / 4
+        grid = self._construct_grid(self.get_colors(high_img_akita_normalized)) #utils.make_grid(self.get_colors(high_img), nrow=2)
         self.logger.experiment.log_image('{}/high_img'.format(phase), self.current_epoch, grid)
 
         plt.clf()
@@ -189,11 +198,10 @@ if __name__ == "__main__":
     print(output)
     print("output shape: ", output.shape)
 
-
     inps_1 = torch.randn((4, 1, 8, 8))
     inps_2 = torch.randn((4, 1, 8, 8))
     tgts = torch.randn((4, 1, 8, 8))
-    dataset = torch.utils.data.TensorDataset(inps_1, inps_2, tgts)
+    dataset = torch.utils.data.TensorDataset(inps_1, inps_2, tgts, tgts)
 
     loader = torch.utils.data.DataLoader(dataset, batch_size=2, num_workers=12, pin_memory=True)
     trainer = pl.Trainer(logger=neptune_logger,
